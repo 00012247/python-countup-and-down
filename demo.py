611260686dcbeeed5b0d3505d3511380e1301b72
00012247 <95014980+00012247@users.noreply.github.com>
2021-11-26 22:06:41 +0500

    demo.py
    CSF homeWork CountDown

---
+++ demo.py
@@ -0,0 +1,18 @@
+#  countdown project
+def countdown(n):
+    if n<=0:
+        print("Blastoff!")
+    else:
+        print(n)
+        countdown(n-1)
+
+countdown(20)
+
+# count up
+def countup(b):
+    if b >=0:
+        print("finish")
+    else:
+        print(b)
+        countup(b+1)
+countup(-20);
